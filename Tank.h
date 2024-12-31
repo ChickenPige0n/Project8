c1c06736b215341ae8d46910b4f50840c9c2e0e9
ChickenPige0n <2336983354@qq.com>
2024-12-31 13:23:09 +0800

    fix: tank won't disappear properly

--- Tank.h
+++ Tank.h
@@ -5,40 +5,39 @@
 #include "Item.h"
 #include <cstdlib>
 class Tank : public Item {
-    bool disabled;
+    bool is_out;
     bool is_super;
 
   public:
-    Tank(Game *g, size_t row) : Item(g), disabled(false) {
-        row = 3;
-        col = 2;
-        srand(time(0));
+    Tank(Game *g, int r, int c) : Item(g), is_out(false) {
+        row = r;
+        col = c;
     }
     void disable() {
         if (is_super) return;
-        disabled = false;
+        is_out = true;
     }
 
     void update(int key) {
-        if (disabled) return;
+        if (is_out) return;
         int randomNumber = rand() % 4;
         switch (randomNumber) {
-            case 1:
+            case 0:
                 if (col > 0) {
                     col--;
                 }
                 break;
-            case 2:
+            case 1:
                 if (col < 80) {
                     col++;
                 }
                 break;
-            case 3:
+            case 2:
                 if (row > 0) {
                     row--;
                 }
                 break;
-            case 4:
+            case 3:
                 if (row < 30) {
                     row++;
                 }
@@ -46,17 +45,17 @@ class Tank : public Item {
         }        
 
         randomNumber = rand() % 99;
-        if (randomNumber <= 2) {
+        if (randomNumber <= 20) {
             game->add_bomb(row, col);
         }
 
         game->paintat(row, col-1,  'C');
         game->paintat(row, col,  is_super ? 'X' : 'O');
 
-        game->paintat(row, col,  'D');
+        game->paintat(row, col+1,  'D');
     }
     bool out() {
-        return false;
+        return is_out;
     }
 };
 #endif
