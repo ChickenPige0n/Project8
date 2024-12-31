8555381552fbb35332858a3c19c7c4d72b3d85bf
ChickenPige0n <2336983354@qq.com>
2024-12-31 19:56:00 +0800

    feat: implement super tank behaviour

--- Tank.h
+++ Tank.h
@@ -15,45 +15,69 @@ class Tank : public Item {
         col = c;
     }
     void disable() {
-        if (is_super) return;
+        if (is_super)
+            return;
         is_out = true;
     }
 
-    void update(int key) {
-        if (is_out) return;
+    void normal_update() {
         int randomNumber = rand() % 4;
         switch (randomNumber) {
-            case 0:
-                if (col > 0) {
-                    col--;
-                }
-                break;
-            case 1:
-                if (col < 80) {
-                    col++;
-                }
-                break;
-            case 2:
-                if (row > 0) {
-                    row--;
-                }
-                break;
-            case 3:
-                if (row < 30) {
-                    row++;
-                }
-                break;
-        }        
+        case 0:
+            if (col > 0) {
+                col--;
+            }
+            break;
+        case 1:
+            if (col < 80) {
+                col++;
+            }
+            break;
+        case 2:
+            if (row > 0) {
+                row--;
+            }
+            break;
+        case 3:
+            if (row < 30) {
+                row++;
+            }
+            break;
+        }
+    }
+    void super_update() {
+        auto p = game->player;
+        auto x_diff = p->row - row;
+        auto y_diff = p->col - col;
+        if (abs(x_diff) > abs(y_diff)) {
+            if (x_diff > 0) {
+                row++;
+            } else {
+                row--;
+            }
+        } else {
+            if (y_diff > 0) {
+                col++;
+            } else {
+                col--;
+            }
+        }
+    }
 
-        randomNumber = rand() % 99;
+    void update(int key) {
+        if (is_out)
+            return;
+
+        is_super ? super_update() : normal_update();
+
+        int randomNumber = rand() % 99;
         if (randomNumber <= 2) {
             game->add_bomb(row, col);
         }
 
-        game->paintat(row, col-1,  'C');
-        game->paintat(row, col,  is_super ? 'X' : 'O');
-
-        game->paintat(row, col+1,  'D');
+        game->paintat(row, col - 1, 'C');
+        game->paintat(row, col, is_super ? 'X' : 'O');
+        game->paintat(row, col + 1, 'D');
     }
     bool out() {
         return is_out;
